229a4e40d3f4447062ac2ec847a8adcc9f7bb22b
andreyground2000 <andreygrigoriev555@gmail.com>
2021-08-09 14:33:22 +0300

    created first route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import express, {Request, Response} from "express";
+import { router } from "./routes/loginRoutes";
+
+const app = express();
+
+// app.get("/", (req: Request, resp: Response) => {
+//     resp.send(`
+//     <div>
+//         <h1>Hey there!</h1>
+//     </div>
+//     `);
+// });
+app.use(router);
+
+app.listen(3000, () => {
+    console.log("hello there")
+})
